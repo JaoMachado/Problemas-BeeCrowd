16a3def51b7ec2985ae8b674a800bce2dd5f4ed8
JaoMachado <jp.dausi@hotmail.com>
2023-08-08 14:39:38 -0300

    Até ex7 pronto

---
+++ Ex4.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+ 
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+        Scanner teclado = new Scanner(System.in);
+        
+        int n1 = teclado.nextInt();
+        int n2 = teclado.nextInt();
+        
+        int prod = n1 * n2;
+        
+        System.out.println("PROD = " + prod);
+    }
+ 
+}
